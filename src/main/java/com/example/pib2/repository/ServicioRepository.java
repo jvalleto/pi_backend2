279cdb840e77498133573310c22001327a67764f
unknown <ta.vorx115@gmail.com>
2025-08-30 14:58:25 -0500

    Se crean las clases de Appointment en util,service,entiti,dto,controller,config

--- src/main/java/com/example/pib2/repository/ServicioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.pib2.repository;
-
-import com.example.pib2.model.entity.Servicio;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ServicioRepository extends JpaRepository<Servicio, Long> {
-}
