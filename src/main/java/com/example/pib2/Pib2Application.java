4ac3384726cf1d4c16b63d2e3303c172e598219a
unknown <ta.vorx115@gmail.com>
2025-09-01 17:03:19 -0500

    se  agregar la  entidad Appointment, se crea la car y configuración de conexión con Supabas

--- src/main/java/com/example/pib2/Pib2Application.java
+++ src/main/java/com/example/pib2/Pib2Application.java
@@ -1,18 +1,17 @@
 package com.example.pib2;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = "com.example.pib2")
+@SpringBootApplication
+@EntityScan(basePackages = "com.example.pib2.models.entity")
 @EnableJpaRepositories(basePackages = "com.example.pib2.repository")
-@EntityScan(basePackages = "com.example.pib2.model.entity")
-
 public class Pib2Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Pib2Application.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(Pib2Application.class, args);
+    }
 }
